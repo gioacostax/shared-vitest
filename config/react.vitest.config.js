0297bb2060b19fa41dae2df084fa9eb1ec502c62
gioacostax <gioacostax@gmail.com>
2024-10-21 08:21:17 -0500

    config

--- config/react.vitest.config.js
+++ config/react.vitest.config.js
@@ -5,19 +5,21 @@
 
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vitest/config';
 
 /**
  * Vitest React config dependencies
  * - @vitejs/plugin-react
  * - vite
+ * - vite-plugin-svgr
  * - @testing-library/dom
  * - @testing-library/jest-dom
  * - @testing-library/react
  * - @testing-library/user-event
  */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     setupFiles: [resolve(__dirname, './react.setup.tsx')],
   },
